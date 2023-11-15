38835ef75f43e2454d49700a286803f6c20bde11
Nima <74507096+Nimausfi@users.noreply.github.com>
2023-11-14 16:32:01 -0800

    Initial commit

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,7 +3,7 @@ from transformers import AutoProcessor, BarkModel
 from IPython.display import Audio
 
 # About App 
-st.title("Text to Speech with Bark")
+st.title("Text to Speech Converter")
 st.write("Enter text and select a speaker for speech generation!")
 
 # Load model
@@ -12,16 +12,16 @@ model = BarkModel.from_pretrained("suno/bark")
 
 # Voice presets options
 voice_presets = {
-    "English Speaker 0": "v2/en_speaker_0",
-    "English Speaker 1": "v2/en_speaker_1",
-    "English Speaker 2": "v2/en_speaker_2",
-    "English Speaker 3": "v2/en_speaker_3",
-    "English Speaker 6": "v2/en_speaker_6",
-    "Spanish Speaker 0": "v2/es_speaker_0",
-    "French Speaker 0": "v2/fr_speaker_0",
-    "Italian Speaker 0": "v2/it_speaker_0",
-    "Japanese Speaker 0": "v2/ja_speaker_0",
-    "Chinese Speaker 0": "v2/zh_speaker_0",
+    "English Speaker 1": "v2/en_speaker_0",
+    "English Speaker 2": "v2/en_speaker_1",
+    "English Speaker 3": "v2/en_speaker_2",
+    "English Speaker 4": "v2/en_speaker_3",
+    "English Speaker 5": "v2/en_speaker_6",
+    "Spanish Speaker": "v2/es_speaker_0",
+    "French Speaker": "v2/fr_speaker_0",
+    "Italian Speaker": "v2/it_speaker_0",
+    "Japanese Speaker": "v2/ja_speaker_0",
+    "Chinese Speaker": "v2/zh_speaker_0",
 }
 
 input_text = st.text_area("Enter text here", "Hello, I wanted to let you know that Bark is amazing!")
@@ -41,6 +41,5 @@ if st.button("Generate Speech"):
     sample_rate = model.generation_config.sample_rate
     st.audio(audio_array, format="audio/wav", sample_rate=sample_rate)
 
-st.write("You can enter any text in the box above and select a speaker from the dropdown menu.")
 st.write("Click 'Generate Speech' to hear the generated audio.")
 st.write("Note: Depending on the length of the text, it may take a moment to generate the speech.")

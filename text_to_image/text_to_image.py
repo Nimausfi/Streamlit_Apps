29c2c216f1c4db0938825d31fd38a9ee68fda7e0
Nima <74507096+Nimausfi@users.noreply.github.com>
2023-11-22 12:46:02 -0800

    Initial commit

--- text_to_image/text_to_image.py
+++ text_to_image/text_to_image.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+from authtoken import auth_token
 import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
@@ -12,3 +13,22 @@ pipe.to(device)
 guidance_scale = 4.5
 temperature = 1.0
 top_k = 50
+
+st.title("Text to Image App")
+
+prompt = st.text_area("Enter your text prompt:", height=5)
+
+def generate_image():
+    with autocast(device):
+        # Adjust parameters for image generation
+        image = pipe(prompt, guidance_scale=guidance_scale, temperature=temperature, top_k=top_k).images[0]
+
+    image_path = 'generatedimage.png'
+    image.save(image_path)
+
+    st.image(image, caption="Generated Image", use_column_width=True)
+
+    st.markdown(f"Download the generated image: [Download]({image_path})")
+
+if st.button("Generate"):
+    generate_image()
